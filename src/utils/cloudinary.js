b5230141fae03c4e539bf843d743f51dc3c08a49
pumpumx <trishuchibhot14@gmail.com>
2025-03-29 23:23:27 +0530

    Configuration setting Completed

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dtvpqxbsd', 
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+    const uploadOnCloudinary = async (localFilePath)=> {
+        try {
+            if(!localFilePath) return null;
+            const response = await cloudinary.uploader.upload(localFilePath , {
+                resource_type: "auto"
+            })
+            console.log("File is uploaded on cloudinary")
+            console.log(respone.url)
+            return response
+        } catch (error) {
+            fs.unlinkSync(localFilePath)  //Will remove locally saved temperory file as the upload operation got failed
+            return {err:error}
+        }
+    }
+
+    export default uploadOnCloudinary
