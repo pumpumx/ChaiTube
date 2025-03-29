bc37eb2792fbd0166fbd41706bb0a20cb4e3bd52
pumpumx <trishuchibhot14@gmail.com>
2025-03-29 22:06:55 +0530

    User Model and Video model coded

--- src/index.js
+++ src/index.js
@@ -7,3 +7,15 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    try {
+        app.listen(process.env.PORT || 8000 , ()=>{
+            console.log(`Server is running at port${process.env.PORT} `)
+        })
+    } catch (error) {
+        console.log("App listening ERROR:: " , err)
+    }
+})
+.catch((err) => {
+    console.log("MongoDB ERROR:: Connection Failed", err)
+})
---
+++ src/utils/apiError.js
@@ -0,0 +1,22 @@
+class ApiError extends apiError {
+    constructor(statusCode , 
+        message="Something went wrong",
+        errors=[],
+        stack=""){
+        super(message)
+        this.statusCode = statusCode
+        this.data=null
+        this.message=null
+        this.success=false
+        this.errors = errors
+
+        if(stack){
+            this.stack = stack
+        }
+        else{
+            Error.captureStackTrace(this , this.constructor)
+        }
+    }
+}
+
+export {ApiError}
